1547d7772d48cec7708253c440010a66c37cacc5
Inatan Lopes Hertzog <inatan_hertzog@hotmail.com>
2014-05-20 00:14:19 -0300

    Interfaces de cadastro e reserva prontas

---
+++ PBR Rent a car/Cadastro Modelo.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace PBR_Rent_a_car
+{
+    public partial class Cadastro_Modelo : Form
+    {
+        public Cadastro_Modelo()
+        {
+            InitializeComponent();
+        }
+    }
+}
